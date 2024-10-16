d3efc207cf55ef25ca13795a29cf0e9a730b09b2
MrReaper46 <129411428+MrReaper46@users.noreply.github.com>
2024-10-16 23:11:33 +0700

    3

--- Assets/Week 4/Scripts/ANDPractice.cs
+++ Assets/Week 4/Scripts/ANDPractice.cs
@@ -80,8 +80,8 @@ public class ANDPractice : MonoBehaviour
     void BaiTap3()
     {
         // Nhập điểm trung bình, điểm hoạt động ngoại khóa, và kiểm tra kỷ luật
-        float avgscore = Random.Range(0.0f, 10.00001f);
-        float extrascore = Random.Range(0.0f, 10.00001f);
+        float avgScore = Random.Range(0.0f, 10.00001f);
+        float extraScore = Random.Range(0.0f, 10.00001f);
         bool violation = Random.Range(0, 2) == 0;
         string kiluat;
         if (violation == true)
@@ -89,9 +89,9 @@ public class ANDPractice : MonoBehaviour
             kiluat = "co";
         }
         else kiluat = "khong";
-        Debug.Log($"Bai Tap 3: Diem TB: {avgscore} - Diem ngoai khoa: {extrascore} - Vi pham ky luat: {kiluat}");
+        Debug.Log($"Bai Tap 3: Diem TB: {avgScore} - Diem ngoai khoa: {extraScore} - Vi pham ky luat: {kiluat}");
         // Kiểm tra nếu điểm trung bình >= 9.0, điểm ngoại khóa >= 8.0, và không vi phạm kỷ luật
-        if (avgscore >= 9.0f && extrascore >= 8.0f && violation != true) Debug.Log("SV du dieu kien nhan hoc bong");
+        if (avgScore >= 9.0f && extraScore >= 8.0f && violation != true) Debug.Log("SV du dieu kien nhan hoc bong");
         else Debug.Log("SV khong du dieu kien nhan hoc bong");
     }
 
@@ -99,21 +99,21 @@ public class ANDPractice : MonoBehaviour
     void BaiTap4()
     {
         // Nhập thông tin email, xác nhận qua điện thoại, và phí tham gia
-        bool emailconfirm = Random.Range(0, 2) == 0;
+        bool emailConfirm = Random.Range(0, 2) == 0;
         string email;
-        if (emailconfirm == true) email = "hop le";
+        if (emailConfirm == true) email = "hop le";
         else email = "khong hop le";
-        bool phoneconfirm = Random.Range(0, 2) == 0;
+        bool phoneConfirm = Random.Range(0, 2) == 0;
         string phone;
-        if (phoneconfirm == true) phone = "hop le";
+        if (phoneConfirm == true) phone = "hop le";
         else phone = "khong hop le";
-        bool feeconfirm = Random.Range(0, 2) == 0;
+        bool feeConfirm = Random.Range(0, 2) == 0;
         string fee;
-        if (feeconfirm == true) fee = "da thanh toan";
+        if (feeConfirm == true) fee = "da thanh toan";
         else fee = "chua thanh toan";
         Debug.Log($"Bai Tap 4: Email: {email} - So dien thoai: {phone} - Phi tham gia: {fee}");
         // Kiểm tra nếu email hợp lệ, đã xác nhận qua điện thoại, và đã thanh toán phí tham gia
-        if(emailconfirm == true&&phoneconfirm == true&& feeconfirm == true) Debug.Log("Dang ki hop le");
+        if(emailConfirm == true&&phoneConfirm == true&& feeConfirm == true) Debug.Log("Dang ki hop le");
         else Debug.Log("Dang ki khong hop le");
     }
 
@@ -121,41 +121,104 @@ public class ANDPractice : MonoBehaviour
     void BaiTap5()
     {
         // Nhập điểm tổng kết, số môn thi trượt, và điểm chuyên cần
+        float score = Random.Range(0.0f, 10.00001f);
+        int failedClass = Random.Range(0, 5);
+        float presentScore = Random.Range(0.0f, 100.00001f);
+        Debug.Log($"Bai Tap 5: Diem tong ket: {score} - So mon truot: {failedClass} - Diem chuyen can: {presentScore}%");
         // Kiểm tra nếu điểm tổng kết < 5.0, số môn trượt <= 2, và điểm chuyên cần > 75%
+        if (score < 5.0f && failedClass <= 2 && presentScore > 75) Debug.Log("Du dieu kien thi lai");
+        else Debug.Log("Khong du dieu kien thi lai");
     }
 
     // Bài Tập 6: Xét Tuyển Nhân Viên
     void BaiTap6()
     {
         // Nhập thông tin về bằng đại học, kinh nghiệm và bài kiểm tra năng lực
+        bool isGraduated = Random.Range(0, 2) == 0;
+        string totNghiep;
+        if (isGraduated == true) totNghiep = "Da tot nghiep";
+        else totNghiep = "Chua tot nghiep";
+        int yearsOfExp = Random.Range(0, 50);
+        bool isTested = Random.Range(0, 2) == 0;
+        string kTNL;
+        if (isTested == true) kTNL = "dau";
+        else kTNL = "khong dau";
+        Debug.Log($"Bai Tap 6: Hoc van DH: {totNghiep} - Kinh nghiem: {yearsOfExp} nam - Bai KTra NL: {kTNL}");
         // Kiểm tra nếu ứng viên đã tốt nghiệp đại học, có ít nhất 2 năm kinh nghiệm, và vượt qua bài kiểm tra năng lực
+        if (isGraduated && yearsOfExp >= 2 && isTested) Debug.Log("Nhan vien du nang luc");
+        else Debug.Log("Nhan vien chua du nang luc");
     }
 
     // Bài Tập 7: Kiểm Tra Điều Kiện Mua Hàng
     void BaiTap7()
     {
         // Nhập số tiền hiện có, kiểm tra hàng trong kho và chương trình khuyến mãi
+        bool moneySufficient = Random.Range(0, 2) == 0;
+        string tien;
+        if (moneySufficient == true) tien = "Du tien";
+        else tien = "Khong du tien";
+        bool isSupplied = Random.Range(0, 2) == 0;
+        string supplied;
+        if (isSupplied == true) supplied = "co";
+        else supplied = "Khong";
+        bool promotionApproved = Random.Range(0, 2) == 0;
+        string kM;
+        if (promotionApproved == true) kM = "Du dieu kien";
+        else kM = "Khong du dieu kien";
+        Debug.Log($"Bai Tap 7: So du: {tien} -  Hang trong kho: {supplied} - Dieu kien tham gia KM: {kM}");
         // Kiểm tra nếu người dùng có đủ tiền, có hàng trong kho, và đủ điều kiện tham gia khuyến mãi
+        if (moneySufficient && isSupplied && promotionApproved) Debug.Log("KH du dieu kien tham gia KM");
+        else Debug.Log("KH khong du dieu kien tham gia KM");
     }
 
     // Bài Tập 8: Đủ Điều Kiện Nhận Phần Thưởng
     void BaiTap8()
     {
         // Nhập thông tin về nhiệm vụ hoàn thành, điểm trung bình nhiệm vụ, và đăng nhập hệ thống
+        int mission = Random.Range(0, 50);
+        float score = Random.Range(0.0f, 10.00001f);
+        bool loggedIn = Random.Range(0, 2) == 0;
+        string dN;
+        if (loggedIn == true) dN = "Co";
+        else dN = "Khong";
+        Debug.Log($"Bai Tap 8: So NV hoan thanh: {mission} - Diem NV: {score} - Da dang nhap:  {dN}");
         // Kiểm tra nếu đã hoàn thành ít nhất 10 nhiệm vụ, điểm nhiệm vụ >= 8, và đăng nhập vào hệ thống trong 7 ngày qua
+        if (mission >= 10 && score >= 8.00f && loggedIn) Debug.Log("Du DK nhan Thuong");
+        else Debug.Log("Khong du DK nhan Thuong");
     }
 
     // Bài Tập 9: Kiểm Tra Điều Kiện Thăng Chức
     void BaiTap9()
     {
         // Nhập số năm làm việc, chỉ tiêu công việc và vi phạm kỷ luật
+        int yearOfWorking = Random.Range(0, 50);
+        bool isPassed = Random.Range(0, 2) == 0;
+        string passed;
+        if (isPassed == true) passed = "Dat chi tieu";
+        else passed = "Khong dat chi tieu";
+        bool violation = Random.Range(0, 2) == 0;
+        string kiluat;
+        if (violation == true) kiluat = "co";
+        else kiluat = "khong";
+        Debug.Log($"Bai Tap 9: So Nam lam viec: {yearOfWorking} - Dat chi tieu cong viec: {passed} - Vi pham ki luat: {kiluat}");
         // Kiểm tra nếu đã làm việc trên 5 năm, đạt chỉ tiêu công việc và không vi phạm kỷ luật
+        if (yearOfWorking >= 5 && isPassed && !violation) Debug.Log("Du dieu kien thang chuc");
+        else Debug.Log("Khong du dieu kien thang chuc");
     }
 
     // Bài Tập 10: Điều Kiện Nhận Vé Miễn Phí
     void BaiTap10()
     {
         // Nhập tuổi, trạng thái thành viên VIP, và số lần mua vé trong năm
+        int age = Random.Range(0, 100);
+        bool isVIP = Random.Range(0, 2) == 0;
+        string vip;
+        if (isVIP == true) vip = "da la thanh vien";
+        else vip = "chua la thanh vien";
+        int year = Random.Range(0, age);
+        Debug.Log($"Bai Tap 10: Tuoi: {age} - VIP: {vip} - So nam: {year}");
         // Kiểm tra nếu người dùng trên 60 tuổi, là thành viên VIP, và đã mua vé ít nhất 3 lần trong năm
+        if (age >= 60 && isVIP && year >= 3) Debug.Log("Du dieu kien nhan ve mien phi");
+        else Debug.Log("Khong du dieu kien nhan ve mien phi");
     }
 }
--- Assets/Week 4/Scripts/ORPractice.cs
+++ Assets/Week 4/Scripts/ORPractice.cs
@@ -24,59 +24,200 @@ public class ORPractice : MonoBehaviour
     void BaiTap1()
     {
         // Kiểm tra xem người dùng có thể vào sự kiện nếu có vé hợp lệ, là thành viên VIP, hoặc được mời
+        bool ticketValid = Random.Range(0, 2) == 0;
+        string ticket;
+        if (ticketValid) ticket = "hop le";
+        else ticket = "khong hop le";
+        bool isVIP = Random.Range(0, 2) == 0;
+        string vip;
+        if (isVIP) vip = "la thanh vien VIP";
+        else vip = "khong phai la thanh vien VIP";
+        bool isInvited = Random.Range(0, 2) == 0;
+        string invited;
+        if (isInvited) invited = "khach duoc moi";
+        else invited = "khong phai khach moi";
+        Debug.Log($"Bai Tap 1: Ve: {ticket} - {vip} - {invited}");
+        if (ticketValid || isVIP || isInvited) Debug.Log("Nguoi dung du dieu kien vao cua");
+        else Debug.Log("Nguoi dung khong du dieu kien vao cua");
     }
 
     // Bài Tập 2: Điều Kiện Trúng Thưởng
     void BaiTap2()
     {
         // Kiểm tra xem người dùng có trúng thưởng nếu số vé của họ trúng giải nhất, nhì, hoặc ba
+        int firstPrice = Random.Range(10000, 99999);
+        int secondPrice = Random.Range(10000, 99999);
+        if(secondPrice == firstPrice) secondPrice = Random.Range(10000, 99999);
+        int thirdPrice = Random.Range(10000, 99999);
+        if (thirdPrice == firstPrice || thirdPrice == secondPrice) thirdPrice = Random.Range(10000, 99999);
+        int user = Random.Range(10000, 99999);
+        Debug.Log($"Bai Tap 2: Giai 1: {firstPrice} - Giai 2: {secondPrice} - Giai 3: {thirdPrice}");
+        if (user == firstPrice || user == secondPrice || user == thirdPrice) Debug.Log($"Ve nguoi dung: {user} - Nguoi dung du dieu kien trung thuong");
+        else Debug.Log($"Ve nguoi dung: {user} - Nguoi dung khong du dieu kien trung thuong");
     }
 
     // Bài Tập 3: Kiểm Tra Điều Kiện Vay Tiền
     void BaiTap3()
     {
         // Kiểm tra xem người dùng có đủ điều kiện vay tiền nếu có thu nhập ổn định, tài sản thế chấp, hoặc người bảo lãnh
+        bool incomeConfirm = Random.Range(0, 2) == 0;
+        string income;
+        if (incomeConfirm) income = "on dinh";
+        else income = "khong on dinh";
+        bool propertyConfirm = Random.Range(0, 2) == 0;
+        string property;
+        if (propertyConfirm) property = "Co tai san the chap";
+        else property = "Khong co tai san the chap";
+        bool hasGuardian = Random.Range(0, 2) == 0;
+        string guardian;
+        if (hasGuardian) guardian = "Co nguoi bao lanh";
+        else guardian = "Khong co nguoi bao lanh";
+        Debug.Log($"Bai Tap 3: Thu nhap: {income} - {property} - {guardian}");
+        if (incomeConfirm || propertyConfirm || hasGuardian) Debug.Log("Nguoi dung du dieu kien vay tien");
+        else Debug.Log("Nguoi dung khong du dieu kien vay tien");
     }
 
     // Bài Tập 4: Điều Kiện Nhận Học Bổng
     void BaiTap4()
     {
         // Kiểm tra xem học sinh có nhận được học bổng nếu có thành tích học tập xuất sắc, hoạt động ngoại khóa tốt, hoặc gia đình khó khăn
+        string[] thanhtichArr = { "Xuat sac", "Tot", "Kha", "Trung Binh", "Yeu" };
+        int randomIndex1 = Random.Range(0, thanhtichArr.Length);
+        int randomIndex2 = Random.Range(0, thanhtichArr.Length);
+        string hocTap = thanhtichArr[randomIndex1];
+        string ngoaiKhoa = thanhtichArr[randomIndex2];
+        bool gdkkConfirm = Random.Range(0, 2) == 0;
+        string gdkk;
+        if (gdkkConfirm) gdkk = "Co";
+        else gdkk = "Khong";
+        Debug.Log($"Bai Tap 4: Thanh tich hoc tap: {hocTap} - Hoat dong ngoai khoa: {ngoaiKhoa} - Gia dinh kho khan: {gdkk}");
+        if (randomIndex1 == 0 || randomIndex2 <= 1 || gdkkConfirm) Debug.Log("Du dieu kien nhan hoc bong");
+        else Debug.Log("Khong du dieu kien nhan hoc bong");
     }
 
     // Bài Tập 5: Kiểm Tra Đăng Ký Sự Kiện
     void BaiTap5()
     {
         // Kiểm tra xem người dùng có thể đăng ký sự kiện nếu có email hợp lệ, số điện thoại xác thực, hoặc đã đăng ký qua trang web
+        bool emailConfirm = Random.Range(0, 2) == 0;
+        string email;
+        if (emailConfirm) email = "Email hop le";
+        else email = "Email khong hop le";
+        bool phoneConfirm = Random.Range(0, 2) == 0;
+        string phone;
+        if (phoneConfirm) phone = "Sdt duoc xac thuc";
+        else phone = "Sdt khong duoc xac thuc";
+        bool isRegistered = Random.Range(0, 2) == 0;
+        string register;
+        if (isRegistered) register = "Da dang ki qua trang web";
+        else register = "chua dang ki qua trang web";
+        Debug.Log($"Bai Tap 5: {email} - {phone} -{register}");
+        if (emailConfirm || phoneConfirm || isRegistered) Debug.Log("Du dieu kien nhan dang ki");
+        else Debug.Log("Khong du dieu kien dang ki");
     }
 
     // Bài Tập 6: Điều Kiện Lái Xe
     void BaiTap6()
     {
         // Kiểm tra xem người dùng có thể lái xe nếu có bằng lái, đã đăng ký xe hợp lệ, hoặc có bảo hiểm xe
+        bool hasLicenced = Random.Range(0, 2) == 0;
+        string licence;
+        if (hasLicenced) licence = "Co bang lai";
+        else licence = "Khong co bang lai";
+        bool carRegistered = Random.Range(0, 2) == 0;
+        string car;
+        if (carRegistered) car = "Xe da duoc dang ki";
+        else car = "Xe chua duoc dang ki";
+        bool hasInsurance = Random.Range(0, 2) == 0;
+        string insurance;
+        if (hasInsurance) insurance = "Co bao hiem xe";
+        else insurance = "Khong co bao hiem xe";
+        Debug.Log($"Bai Tap 6: {licence} - {car} - {insurance}");
+        if (hasLicenced || carRegistered || hasInsurance) Debug.Log("Du dieu kien lai xe");
+        else Debug.Log("Khong du dieu kien lai xe");
     }
 
     // Bài Tập 7: Kiểm Tra Quyền Truy Cập Tài Liệu
     void BaiTap7()
     {
         // Kiểm tra xem người dùng có quyền truy cập tài liệu nếu là quản trị viên, được cấp quyền truy cập từ quản lý, hoặc có mã xác thực
+        bool isAdmin = Random.Range(0, 2) == 0;
+        string admin;
+        if (isAdmin) admin = "Nguoi dung la quan tri vien";
+        else admin = "Nguoi dung khong phai la quan tri vien";
+        bool hasPermission = Random.Range(0, 2) == 0;
+        string permission;
+        if (hasPermission) permission = "Co quyen truy cap";
+        else permission = "Khong co quyen truy cap";
+        bool hasCode = Random.Range(0, 2) == 0;
+        string code;
+        if (hasCode) code = "Co ma xac thuc";
+        else code = "Khong co co ma xac thuc";
+        Debug.Log($"Bai Tap 7: {admin} - {permission} - {code}");
+        if (isAdmin || hasPermission || hasCode) Debug.Log("Co quyen truy cap tai lieu");
+        else Debug.Log("Khong co quyen truy cap tai lieu");
     }
 
     // Bài Tập 8: Kiểm Tra Điều Kiện Tham Gia Khóa Học
     void BaiTap8()
     {
         // Kiểm tra xem học sinh có thể tham gia khóa học đặc biệt nếu có thư giới thiệu từ giáo viên, đã hoàn thành bài kiểm tra đầu vào, hoặc có kinh nghiệm liên quan
+        bool hasRecommendation = Random.Range(0, 2) == 0;
+        string recommendation;
+        if (hasRecommendation) recommendation = "Nguoi dung co thu gioi thieu tu giao vien";
+        else recommendation = "Nguoi dung khong co thu gioi thieu tu giao vien";
+        bool completeTest = Random.Range(0, 2) == 0;
+        string test;
+        if (completeTest) test = "Da hoan thanh bai kiem tra";
+        else test = "Chua hoan thanh bai kiem tra";
+        bool hasExp = Random.Range(0, 2) == 0;
+        string exp;
+        if (hasExp) exp = "Co kinh nghiem";
+        else exp = "Khong co kinh nghiem";
+        Debug.Log($"Bai Tap 8: {recommendation} - {test} - {exp}");
+        if (hasRecommendation || completeTest || hasExp) Debug.Log("Du dieu kien tham gia khoa hoc");
+        else Debug.Log("Khong du dieu kien tham gia khoa hoc");
     }
 
     // Bài Tập 9: Điều Kiện Tải Ứng Dụng Miễn Phí
     void BaiTap9()
     {
         // Kiểm tra xem người dùng có thể tải ứng dụng miễn phí nếu có mã khuyến mãi, là thành viên VIP, hoặc ứng dụng đang trong thời gian miễn phí
+        bool hasPromotion = Random.Range(0, 2) == 0;
+        string promotion;
+        if (hasPromotion) promotion = "Nguoi dung co ma khuyen mai";
+        else promotion = "Nguoi dung khong co ma khuyen mai";
+        bool isVIP = Random.Range(0, 2) == 0;
+        string vip;
+        if (isVIP) vip = "La thanh vien VIP";
+        else vip = "Khong phai la thanh vien VIP";
+        bool hasTrial = Random.Range(0, 2) == 0;
+        string trail;
+        if (hasTrial) trail = "dang dung mien phi";
+        else trail = "khong duoc dung mien phi";
+        Debug.Log($"Bai Tap 9: {promotion} - {vip} - {trail}");
+        if (hasPromotion || isVIP || hasTrial) Debug.Log("Du dieu kien tai ung dung mien phi");
+        else Debug.Log("Khong du dieu kien tai ung dung mien phi");
     }
 
     // Bài Tập 10: Điều Kiện Sử Dụng Dịch Vụ Đặc Biệt
     void BaiTap10()
     {
         // Kiểm tra xem khách hàng có thể sử dụng dịch vụ đặc biệt nếu đã chi tiêu trên mức yêu cầu, là khách hàng thân thiết, hoặc có chương trình khuyến mãi
+        bool spendingValid = Random.Range(0, 2) == 0;
+        string spending;
+        if (spendingValid) spending = "Chi tieu tren muc yeu cau";
+        else spending = "Chi tieu duoi muc yeu cau";
+        bool isRegular = Random.Range(0, 2) == 0;
+        string regular;
+        if (isRegular) regular = "La khach hang than thiet";
+        else regular = "Khong phai la khach hang than thiet";
+        bool hasPromotion = Random.Range(0, 2) == 0;
+        string promotion;
+        if (hasPromotion) promotion = "co khuyen mai";
+        else promotion = "khong co khuyen mai";
+        Debug.Log($"Bai Tap 10: {spending} - {regular} - {promotion}");
+        if (spendingValid || isRegular || hasPromotion) Debug.Log("Du dieu kien su dung dich vu dac biet");
+        else Debug.Log("Khong du dieu kien su dung dich vu dac biet");
     }
 }
