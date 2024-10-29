06ec72a5bcda16ce404067dca6f2de18b2015be6
MrReaper46 <129411428+MrReaper46@users.noreply.github.com>
2024-10-29 14:17:24 +0700

    2

--- Assets/Week 3/Scripts/DamageSystem/DamageSender.cs
+++ Assets/Week 3/Scripts/DamageSystem/DamageSender.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class DamageSender : PersonalBehaviour
 {
     [SerializeField] protected int damage = 1;
+    [SerializeField] protected Collider _collider;
+   
     protected virtual void OnTriggerEnter(Collider collider)
     {
         DamageReceiver damageReceiver = collider.GetComponent<DamageReceiver>();
@@ -12,5 +15,16 @@ public abstract class DamageSender : PersonalBehaviour
 
         damageReceiver.Receive(this.damage, this);
         Debug.Log(transform.name + ": " + collider.name, collider.gameObject);
+    }
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadTriggerCollider();
+    }
+    protected virtual void LoadTriggerCollider()
+    {
+        if (this._collider != null) return;
+        this._collider = GetComponent<Collider>();
+        this._collider.isTrigger = true;
     }
 }
--- Assets/Week 3/Scripts/Effects/Bullet/BulletDamageSender.cs
+++ Assets/Week 3/Scripts/Effects/Bullet/BulletDamageSender.cs
@@ -2,7 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SphereCollider))]
 public class BulletDamageSender : DamageSender
 {
- 
+    [SerializeField] protected SphereCollider sphereCollider;
+    protected override void LoadTriggerCollider()
+    {
+        if (this._collider != null) return;
+        this._collider = GetComponent<Collider>();
+        this.sphereCollider = (SphereCollider)this._collider;
+        this.sphereCollider.isTrigger = true;
+        this.sphereCollider.radius = 0.5f;
+        Debug.Log(transform.name + ": Load" + _collider, gameObject);
+    }
+
+
 }
